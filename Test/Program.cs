98197807f77c132a7523119c310ab59fdb17074b
Sander Vervaet <sander.vervaet@student.hogent.be>
2024-11-07 13:17:42 +0100

    extra comments

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Test
+﻿using System.Text.Json;
+
+namespace Test
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             //Schrijf hieronder een aantal extra comments en commit -> push ze naar de main.
+
+            //test1 
+
+            // comment 2 
+
+            //comment 3 
+
+            // hopelijk is dees genoeg 
+
         }
     }
 }
